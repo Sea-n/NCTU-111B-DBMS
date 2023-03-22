d78c7333369ee3b1d81208b89adff80aa4e3d8aa
Sean Wei <me@sean.taipei>
2023-03-22 23:30:21 +0800

    hw1: optimize Q8

--- HW1-MLB/Q8.sql
+++ HW1-MLB/Q8.sql
@@ -4,39 +4,32 @@ ROUND(9 * AVG(2021_K), 4) AS `2021_avg_K/9`,
 CONCAT(ROUND(AVG(2020_PC), 4), '-', ROUND(AVG(2020_ST), 4)) AS `2020_PC-ST`,
 CONCAT(ROUND(AVG(2021_PC), 4), '-', ROUND(AVG(2021_ST), 4)) AS `2021_PC-ST`
 FROM (  /* 每位投手的統計資料 */
-    SELECT 2020_K, 2020_PC, 2020_ST, 2021_K, 2021_PC, 2021_ST,
-    CASE WHEN team_cnt = 1 THEN 'Unchanged' ELSE 'Changed' END AS Pitcher
+    SELECT 2020_K, 2020_PC, 2020_ST, 2021_K, 2021_PC, 2021_ST, Pitcher
     FROM (  /* 符合條件的投手 */
         SELECT Pitcher_Id,
-        SUM(CASE WHEN YEAR(Date) = 2020 THEN IP ELSE NULL END) AS ip_2020,
-        SUM(CASE WHEN YEAR(Date) = 2021 THEN IP ELSE NULL END) AS ip_2021,
-        COUNT(DISTINCT Team) AS team_cnt
+        SUM(CASE WHEN YEAR(Date) = 2020 THEN IP END) AS ip_2020,
+        SUM(CASE WHEN YEAR(Date) = 2021 THEN IP END) AS ip_2021,
+        CASE WHEN COUNT(DISTINCT Team) = 1 THEN 'Unchanged' ELSE 'Changed' END AS Pitcher
         FROM pitchers
         INNER JOIN games ON pitchers.Game = games.Game
         WHERE YEAR(Date) = 2020 OR YEAR(Date) = 2021
         GROUP BY Pitcher_Id
         HAVING ip_2020 > 0 AND ip_2021 > 0 AND ip_2020 + ip_2021 > 50
-    ) AS p2
+    ) AS p1
     INNER JOIN (  /* 兩個年份分別取值到不同 column */
-        SELECT Pitcher_Id, AVG(K / IP) AS 2020_K,
-        AVG(SUBSTRING(PC_ST, 1, POSITION('-' IN PC_ST))) AS 2020_PC,
-        AVG(SUBSTRING(PC_ST, POSITION('-' IN PC_ST) + 1)) AS 2020_ST
+        SELECT Pitcher_Id,
+        AVG(CASE WHEN YEAR(Date) = 2020 THEN K / IP END) AS 2020_K,
+        AVG(CASE WHEN YEAR(Date) = 2021 THEN K / IP END) AS 2021_K,
+        AVG(CASE WHEN YEAR(Date) = 2020 THEN SUBSTRING(PC_ST, 1, POSITION('-' IN PC_ST)) END) AS 2020_PC,
+        AVG(CASE WHEN YEAR(Date) = 2021 THEN SUBSTRING(PC_ST, 1, POSITION('-' IN PC_ST)) END) AS 2021_PC,
+        AVG(CASE WHEN YEAR(Date) = 2020 THEN SUBSTRING(PC_ST, POSITION('-' IN PC_ST) + 1) END) AS 2020_ST,
+        AVG(CASE WHEN YEAR(Date) = 2021 THEN SUBSTRING(PC_ST, POSITION('-' IN PC_ST) + 1) END) AS 2021_ST
         FROM pitchers
         INNER JOIN games ON pitchers.Game = games.Game
-        WHERE YEAR(Date) = 2020 AND IP > 0
+        WHERE IP > 0
         GROUP BY Pitcher_Id
-    ) AS p0 ON p0.Pitcher_Id = p2.Pitcher_Id
-    INNER JOIN (
-        SELECT Pitcher_Id, AVG(K / IP) AS 2021_K,
-        AVG(SUBSTRING(PC_ST, 1, POSITION('-' IN PC_ST))) AS 2021_PC,
-        AVG(SUBSTRING(PC_ST, POSITION('-' IN PC_ST) + 1)) AS 2021_ST
-        FROM pitchers
-        INNER JOIN games ON pitchers.Game = games.Game
-        WHERE YEAR(Date) = 2021 AND IP > 0
-        GROUP BY Pitcher_Id
-    ) AS p1 ON p1.Pitcher_Id = p2.Pitcher_Id
-    GROUP BY p2.Pitcher_Id
-) AS p3
+    ) AS p2 ON p2.Pitcher_Id = p1.Pitcher_Id
+) AS p0
 GROUP BY Pitcher;  /* 最後輸出 Changed/Unchanged 兩個 row */
 
 /*
