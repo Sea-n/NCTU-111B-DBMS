02362cc86ab3614565f07c443332e987c003b1ab
Sean Wei <me@sean.taipei>
2023-04-19 15:41:30 +0800

    hw2: add simple testdata

--- HW2-Hashing/hash.cpp
+++ HW2-Hashing/hash.cpp
@@ -13,10 +13,14 @@ hash_bucket::hash_bucket(int hash_key, int depth) {
  */
 void hash_bucket::clear() {
     printf("clear()\n");
+	free(this->first);
 }
 
 hash_table::hash_table(int table_size, int bucket_size, int num_rows, vector<int> key, vector<int> value) {
-    printf("hash_table(table_size=%d, bucket_size=%d, num_rows=%d, key[0]=%d, value[0]=%d)\n", table_size, bucket_size, num_rows, key[0], value[0]);
+    printf("hash_table(table_size=%d, bucket_size=%d, num_rows=%d, key=%ld, value=%ld)\n", table_size, bucket_size, num_rows, key.size(), value.size());
+	for (int i=0; i<num_rows; i++) {
+		printf("i=%d: key=%d, value=%d\n", i, key[i], value[i]);
+	}
 }
 
 /*
@@ -24,14 +28,14 @@ hash_table::hash_table(int table_size, int bucket_size, int num_rows, vector<int
  * Furthermore, if the global depth equals to the local depth, you need to extend the table size.
  */
 void hash_table::extend(hash_bucket *bucket) {
-
+	printf("extend(bucket=%p)\n", bucket);
 }
 
 /*
  * When construct hash_table you can call insert() in the for loop for each key-value pair.
  */
 void hash_table::insert(int key, int value) {
-    
+    printf("int(key=%d, value=%d)\n", key, value);
 }
 
 /*
@@ -39,7 +43,7 @@ void hash_table::insert(int key, int value) {
  * Check whether the table necessory need the current size of table, or half the size of table.
  */
 void hash_table::half_table() {
-    
+    printf("half_table()\n");
 }
 
 /*
@@ -49,27 +53,27 @@ void hash_table::half_table() {
  * with pair hash index comes to the same local depth.
  */
 void hash_table::shrink(hash_bucket *bucket) {
-
+	printf("shrink(bucket=%p)\n", bucket);
 }
 
 /*
  * When executing remove_query you can call remove() in the for loop for each key.
  */
 void hash_table::remove(int key) {
-
+	printf("remove(key=%d)\n", key);
 }
 
 void hash_table::key_query(vector<int> query_keys, string file_name) {
-    
+    printf("key_query(query_keys=%ld, file_name=%s)\n", query_keys.size(), file_name.c_str());
 }
 
 void hash_table::remove_query(vector<int> query_keys) {
-    
+    printf("remove_query(query_keys=%ld)\n", query_keys.size());
 }
 
 /*
  * Free the memory that you have allocated in this program.
  */
 void hash_table::clear() {
-
+	printf("clear()\n");
 }
